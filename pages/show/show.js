b9eec8d8e8052c7b233de9818d096af0c4868b62
Youyi Wang <youyi0206@126.com>
2020-08-07 18:44:12 +0800

    fixed userID issue and show page can see review, created a fake button

--- pages/show/show.js
+++ pages/show/show.js
@@ -7,8 +7,10 @@ Page({
    */
   data: {
     currentUser:{},
+    allCard:{},
     productcard:{},
     desc:{},
+    reviews:[]
 
   },
 
@@ -16,29 +18,83 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    console.log(options)
     this.setData({
       currentUser: app.globalData.userInfo,
     });
 
     const ProductCard = new wx.BaaS.TableObject('productCard');
-    const Index = new wx.BaaS.TableObject('Index');
-    ProductCard.find().then((res) => {
-      console.log(res)
-      this.setData({
-        productcard:res.data.objects
-      })
+    const Reviews = new wx.BaaS.TableObject('review');
+
+    Reviews.find().then((res)=>{
+      console.log('reviewTable', res)
     })
 
+  
+    //show one event detail
+    /*ProductCard.find().then((res) => {
+      console.log(res)
+      this.setData({
+        allCard:res.data.objects
+      })
+    })*/
 
-   /* //show one event
+
+   //show one event
     ProductCard.get(options.id).then((res)=>{
       console.log(res);
       this.setData({
-        productcard: res,
+        productcard: res.data,
       })
-    });*/
+    });
+
+  //pull corresponding reviews
+  let query = new wx.BaaS.Query();
+  query.compare('product_id', '=', options.id);
+  Reviews.setQuery(query).find().then((res)=>{
+    console.log('queried object',res)
+    this.setData({
+      reviews: res.data.objects,
+    })
+  });
+
 
   },
+
+    //create a new comment
+  createReview:function(event){
+    console.log('create review',event);
+    const content = event.detail.value.content;
+
+    let Review = new wx.BaaS.TableObject('review');
+    let newReview = Review.create();
+    console.log('what is data', this.data)
+    const data = {
+      product_id: this.data.restaurant.id,
+      review: content,
+    }
+    
+    newReview.set(data);
+    newReview.save().then((res)=>{
+      console.log('save res',res);
+      const newReviews = this.data.comments;
+      newReviews.push(res.data);
+
+      this.setData({
+        comments:newReviews,
+
+      })
+    })
+
+    
+  },
+
+
+
+
+
+
+
 
   /**
    * Lifecycle function--Called when page is initially rendered
